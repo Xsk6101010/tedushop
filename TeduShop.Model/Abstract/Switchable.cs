d8f759b1a31d28dfef2bbe34d7229880991ddc51
Hoàng An Tâm <hjhjcallme1@gmail.com>
2018-05-04 18:18:02 +0700

    add domain entities for project

---
+++ TeduShop.Model/Abstract/Switchable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+    public class Switchable : ISwitchable
+    {
+        public bool Status
+        {
+            set;get;
+        }
+    }
+}
---
+++ TeduShop.Model/Models/Footer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Data.Models
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { set; get; }
+
+        [Required]
+        public string Contennt { set; get; }
+    }
+}
